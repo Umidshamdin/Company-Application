e65a635570a0aee0d8f8a5252013c238d81b516f
[umid shamdinli] <[umidash@code.edu.az]>
2021-12-23 11:47:24 +0400

    Projects were created

---
+++ Repository/Implementation/CompanyRepository.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Models;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Implementation
+{
+    public class CompanyRepository : IRepository<Company>
+    {
+        public bool Create(Company entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Company entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Get(Predicate<Company> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Company> GetAll(Predicate<Company> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Company entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
